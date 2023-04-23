f05e238a4181dfb183296e6db97f300c15b18d68
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-04-23 16:34:37 +0200

    ok

---
+++ exeample/src/main/java/Codigo/TipoInasistencia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Codigo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author sulbaranjc
+ */
+
+@Data
+public class TipoInasistencia {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+}
