b277c38e0fbf95eff29a3e01387bce065c9e9491
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-05-02 12:58:25 +0200

    asistencia en desarrollo
    prototipo de interfaz

---
+++ exeample/src/main/java/com/example/controller/ControllerAsistencia.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author sulbaranjc
+ */
+@Controller
+@RequestMapping("/asistencia")
+
+public class ControllerAsistencia {
+    
+   @GetMapping("/crud")
+ public String crud(Model model){
+        String valorfinal="./asistencia/asistencia";
+return valorfinal;
+  }
+    
+
+}
