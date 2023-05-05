ea66b5ecf3c03272e4b8c5f7fdc91271eda02750
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-05-05 12:05:04 +0200

    bug y otros
    ok

--- exeample/src/main/java/Codigo/Alumno.java
+++ exeample/src/main/java/Codigo/Alumno.java
@@ -13,6 +13,7 @@ import lombok.Data;
 @Data
 public class Alumno {
     private int id;
+    private String dni;    
     private String nombre;
     private String apellido;
     private String correo;
